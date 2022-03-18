071706c47c6015f2747fe5e6e317ea494c854719
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-03-18 20:03:09 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.99%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -6,8 +6,8 @@ public:
         int move_by = n-1;
         
         for(int i = 0 ; i < n; i++){
-            for(int j = i ; j < n; j++){    
-                swap(matrix[j][i],matrix[i][j]);
+            for(int j = i+1 ; j < n; j++){    
+                swap(matrix[i][j],matrix[j][i]);
             }
     
         reverse(matrix[i].begin(), matrix[i].end());

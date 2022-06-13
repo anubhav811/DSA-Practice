ef25a06b369f399823200f02f40d32fcc9afa039
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-14 04:00:12 +0530

    Time: 4 ms (50.69%), Space: 8.9 MB (99.18%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -6,8 +6,8 @@ public:
         int s2 = n-1;
         int ind = m+n-1;
         
-        while(s2>=0){
-            if(s1>=0 && nums1[s1]>nums2[s2] ){
+        while(s2>=0 && s1>=0){
+            if(nums1[s1]>nums2[s2] ){
                 nums1[ind]=nums1[s1];
                 s1=s1-1;
                 ind= ind-1;
@@ -17,6 +17,10 @@ public:
                 ind= ind-1;
             }
         }
-        
+        while(s2>=0){
+            nums1[ind] = nums2[s2];
+            ind = ind-1;
+            s2= s2-1;
+            }
     }
 };

23c7df8eabfdd8fa57ebe380b48c7e600ae71d5f
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-14 01:29:50 +0530

    Time: 14 ms (77.22%), Space: 10.9 MB (46.17%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,38 +3,39 @@ public:
      vector<int> twoSum(vector<int> &nums, int target)
     {   
          
-         vector<pair<int,int>> v;
-         for(int i =0 ;i<nums.size();i++){ 
-             v.push_back({nums[i],i});
-         }
-         sort(v.begin(),v.end());
-         int s = 0;
-         int e = nums.size()-1;
-         while(s<e){
-             if(v[s].first + v[e].first == target){
-                 return {v[s].second,v[e].second};
-             }
-             else if(v[s].first + v[e].first > target){
-                 e--;
-             }
-             else{
-                 s++;
-             }
-         }
-         
-         return {-1,-1};
-         
-//          unordered_map<int,int> m;
-//          for (int i = 0; i < nums.size(); i++)
-//         {
-//              int x = nums[i]           ;  //  Current value
-//            int y=target-x;
-//              if(m.find(y)!=m.end()){       // check if "key" i.e the value according to array exists in the map
-//                  return {i,m[y]};
+         // approach 3
+//          vector<pair<int,int>> v;
+//          for(int i =0 ;i<nums.size();i++){ 
+//              v.push_back({nums[i],i});
+//          }
+//          sort(v.begin(),v.end());
+//          int s = 0;
+//          int e = nums.size()-1;
+//          while(s<e){
+//              if(v[s].first + v[e].first == target){
+//                  return {v[s].second,v[e].second};
 //              }
-//              m[x]=i;                       // then insert anyways.
-//         }
+//              else if(v[s].first + v[e].first > target){
+//                  e--;
+//              }
+//              else{
+//                  s++;
+//              }
+//          }
+         
+//          return {-1,-1};
+         
+         unordered_map<int,int> m;
+         for (int i = 0; i < nums.size(); i++)
+        {
+             int x = nums[i]           ;  //  Current value
+           int y=target-x;
+             if(m.find(y)!=m.end()){       // check if "key" i.e the value according to array exists in the map
+                 return {i,m[y]};
+             }
+             m[x]=i;                       // then insert anyways.
+        }
         
-//         return {-1,-1};
+        return {-1,-1};
      }
 };

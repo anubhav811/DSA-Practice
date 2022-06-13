89ad92851e334a4744de057594dd20df292e2805
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-14 01:30:39 +0530

    Time: 15 ms (74.62%), Space: 10.8 MB (52.90%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,8 +2,21 @@ class Solution {
 public:
      vector<int> twoSum(vector<int> &nums, int target)
     {   
+          unordered_map<int,int> m;
+         for (int i = 0; i < nums.size(); i++)
+        {
+             int x = nums[i]           ;  //  Current value
+           int y=target-x;
+             if(m.find(y)!=m.end()){       // check if "key" i.e the value according to array exists in the map
+                 return {i,m[y]};
+             }
+             m[x]=i;                       // then insert anyways.
+        }
+        
+        return {-1,-1};
+     }
          
-         // approach 3
+//          APPROACH 3
 //          vector<pair<int,int>> v;
 //          for(int i =0 ;i<nums.size();i++){ 
 //              v.push_back({nums[i],i});
@@ -25,17 +38,5 @@ public:
          
 //          return {-1,-1};
          
-         unordered_map<int,int> m;
-         for (int i = 0; i < nums.size(); i++)
-        {
-             int x = nums[i]           ;  //  Current value
-           int y=target-x;
-             if(m.find(y)!=m.end()){       // check if "key" i.e the value according to array exists in the map
-                 return {i,m[y]};
-             }
-             m[x]=i;                       // then insert anyways.
-        }
         
-        return {-1,-1};
-     }
 };

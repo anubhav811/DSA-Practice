f43a910f8289e86d1faddd0e23b02f822f52e8dd
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-16 01:19:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.11%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,12 +2,25 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-      vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+        // Combination formula (Optimal approach)
+        
+        int N = m+n-2;
+        int r = m-1;
+        double res = 1;
+        
+        for(int i=1 ; i<=r ; i++){
+            res=res*(N-r+i)/i;
         }
-        return dp[m - 1][n - 1];
+        
+        return res;
     }
+        // Using Dp for the recursive solution (Second approach)
+    //   vector<vector<int>> dp(m, vector<int>(n, 1));
+    //     for (int i = 1; i < m; i++) {
+    //         for (int j = 1; j < n; j++) {
+    //             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    //         }
+    //     }
+    //     return dp[m - 1][n - 1];
+    // }
 };

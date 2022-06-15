28dcd318b229462f9156da2fb7c9c9d0909bae80
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-15 21:22:44 +0530

    Time: 34 ms (28.24%), Space: 19.5 MB (58.85%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        map<int,int> m;
+        int major = 0;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto i : m){
+            if(i.second >(nums.size()/2)){
+                major = max(major,i.first);
+            }
+         }
+        return major;
+    }
+};

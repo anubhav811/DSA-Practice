7d69542ec123248126d3bf4216b4c3ccda8f9e74
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-23 16:11:27 +0530

    Time: 6 ms (17.95%), Space: 7.1 MB (71.31%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,22 +11,32 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        // tortoise hare approach
         
-        ListNode* temp1 = head;
-        int l = 0;
-        while(temp1!=NULL){
-            temp1 = temp1->next;
-            l++;
+        ListNode* slow = head; 
+        ListNode* fast = head;
+        while (fast!=NULL && fast->next!=NULL){
+            slow = slow->next; 
+            fast = fast->next->next;
         }
+        return slow;
 
-        int mid = l/2;
+        // Approach 1 : Find length and then find mid and then traverse
+//         ListNode* temp1 = head;
+//         int l = 0;
+//         while(temp1!=NULL){
+//             temp1 = temp1->next;
+//             l++;
+//         }
+
+//         int mid = l/2;
         
-        ListNode* temp2 = head;
-        int c = 0;
-        while(c<mid){
-            temp2 = temp2->next;
-            c++;
-        }
-        return temp2;
+//         ListNode* temp2 = head;
+//         int c = 0;
+//         while(c<mid){
+//             temp2 = temp2->next;
+//             c++;
+//         }
+//         return temp2;
     }
 };

e7620eea9c3bfc0609005a320ce7102f28aefef2
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-23 12:05:46 +0530

    Time: 4 ms (96.19%), Space: 8.4 MB (57.80%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string str) {
+    int lengthOfLongestSubstring(string s) {
     
-      int maxans = INT_MIN;
-  unordered_set < int > set;
-  int l = 0;
-  for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
-  {
-    if (set.find(str[r]) != set.end()) //if duplicate element is found
-    {
-      while (l < r && set.find(str[r]) != set.end()) {
-        set.erase(str[l]);
-        l++;
-      }
-    }
-    set.insert(str[r]);
-    maxans = max(maxans, r - l + 1);
-  }
-        if(maxans==INT_MIN){
-            return 0;
-        }
-  return maxans;
+        // Optimised approach 2 : Use one pointer alongwith map to store char and its last occurence index . When L has to move it will only move +1 the last occurence index saving time 
+        
+      vector < int > mpp(256, -1);               // Because there can be 256 characters  (ASCII)
 
-    
-    }
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)             // if found previously i.e exists in map 
+          left = max(mpp[s[right]] + 1, left);       // move L to +1 the found index to get left index of substring  range
+
+        mpp[s[right]] = right;                      // update the value in map
+
+        len = max(len, right - left + 1);            // calculate length of string 
+        right++;                                     // move R ahead
+      }
+      return len;
+    }       
+        
+        // Optimized approach 1 :  Use two pointers alongwith hash set
+
+//       int maxans = INT_MIN;
+//   unordered_set < int > set;
+//   int l = 0;
+//   for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
+//   {
+//     if (set.find(str[r]) != set.end()) //if duplicate element is found
+//     {
+//       while (l < r && set.find(str[r]) != set.end()) {
+//         set.erase(str[l]);                   // move left pointer ahead 
+//         l++;
+//       }
+//     }
+//     set.insert(str[r]);                  // insert the char in hashset
+//     maxans = max(maxans, r - l + 1);
+//   }
+//         if(maxans==INT_MIN){
+//             return 0;
+//         }
+//   return maxans;
+// }
 };

68cebde37fbd22bc318c35390487dabbaf5a08b9
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-06-23 13:43:01 +0530

    Time: 12 ms (30.48%), Space: 8.4 MB (20.38%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,40 +11,38 @@
 class Solution {
 public:
     ListNode* iterative(ListNode* &head)
-    {
-        if(head==NULL || head->next == NULL) return head;
-        
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        ListNode* forward;
-        while(curr != NULL){
-            forward = curr->next;
-            curr->next = prev;
-            prev=curr;
-            curr=forward;
-            }
-        head = prev;
-        return head;
-    }
+        {
+            if(head==NULL || head->next == NULL) return head;
+            ListNode* prev = NULL;
+            ListNode* curr = head;
+            ListNode* forward;
+            while(curr != NULL){
+                forward = curr->next;
+                curr->next = prev;
+                prev=curr;
+                curr=forward;
+                }
+            head = prev;
+            return head;
+        }
        
     ListNode* recursive(ListNode* &head)
-    {
-        if(head==NULL || head->next == NULL) return head;
-        
-        ListNode* newHead = recursive(head->next);
-        
-        head->next->next = head;
-        head->next = NULL;
-        return newHead;
-    }
+        {
+            if(head==NULL || head->next == NULL) return head;
+            ListNode* newHead = recursive(head->next);
+            head->next->next = head;
+            head->next = NULL;
+            return newHead;
+        }
     
     ListNode* reverseList(ListNode* head) {
         
+        // Approach 2
+        return recursive(head);
+
         // Approach 1
         // return iterative(head);
         
-        // Approach 2
-        return recursive(head);
 
     }
 };

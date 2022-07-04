00d4c892332e0333aa70caca2c398428874c4771
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-05 03:46:31 +0530

    Time: 351 ms (48.01%), Space: 118.1 MB (48.75%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-ListNode* reverse(ListNode* ptr) {
+    ListNode* reverse(ListNode* ptr) {
     ListNode* pre=NULL;
     ListNode* nex=NULL;
     while(ptr!=NULL) {
@@ -44,4 +44,23 @@ bool isPalindrome(ListNode* head) {
     return true;
 }
 
+//    Approach 1 
+    
+    //bool isPalindrome(ListNode* head) {
+        
+        
+//         vector<int> arr;
+//         while(head != NULL) {
+//             arr.push_back(head->val);
+//             head = head->next;
+//         }
+//         for(int i=0;i<arr.size()/2;i++){
+//             if(arr[i] != arr[arr.size()-i-1]){
+//                 return false; 
+//             }
+//         }
+        
+//         return true;
+
+//     }
 };

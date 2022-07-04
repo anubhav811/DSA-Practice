83d40f6eedc4b514d5d2499368f59904dcdc8679
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-05 03:45:48 +0530

    Time: 267 ms (83.10%), Space: 117.9 MB (61.63%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,17 +10,38 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        
-        vector<int> arr;
-        while(head != NULL) {
-            arr.push_back(head->val);
-            head = head->next;
-        }
-        for(int i=0;i<arr.size()/2;i++) 
-            if(arr[i] != arr[arr.size()-i-1]) 
-                return false;
-        return true;
-
+ListNode* reverse(ListNode* ptr) {
+    ListNode* pre=NULL;
+    ListNode* nex=NULL;
+    while(ptr!=NULL) {
+        nex = ptr->next;
+        ptr->next = pre;
+        pre=ptr;
+        ptr=nex;
     }
+    return pre;
+}
+
+bool isPalindrome(ListNode* head) {
+    if(head==NULL||head->next==NULL) return true;
+        
+    ListNode* slow = head;
+    ListNode* fast = head;
+        
+    while(fast->next!=NULL&&fast->next->next!=NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+        
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+        
+    while(slow!=NULL) {
+        if(head->val != slow->val) return false;
+        head = head->next;
+        slow = slow->next;
+    }
+    return true;
+}
+
 };

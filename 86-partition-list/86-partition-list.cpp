ede21310ce7057f73bf811e55b8f6aa7a7d20e28
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-22 16:54:06 +0530

    Time: 2 ms (96.47%), Space: 10.3 MB (73.43%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -12,30 +12,61 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         
-        ListNode* temp  = head;
-        vector<int> krina ; 
+        // Approach 2 : Two Pointers  O(1)
         
-        
-        while(temp!= NULL){
-            krina.push_back(temp->val);
-            temp = temp ->next; 
-        }
-        
-        temp = head;
-        for(int  i = 0 ; i < krina.size();i++){
-            if(krina[i]<x){
-                temp->val = krina[i];
-                temp = temp->next;
+        ListNode* less  = new ListNode(0);
+        ListNode* great = new ListNode(0);
+        ListNode* less_head = less;
+        ListNode* great_head = great;
+
+    
+        while(head!=NULL)
+        {
+            if(head->val < x){
+                less->next = head;
+                less = less->next;
+                head = head->next;
+                less -> next = NULL;
             }
-        }
-        for(int  i = 0 ; i < krina.size();i++){
-            if(krina[i]>=x){
-                temp->val = krina[i];
-                temp = temp->next;
+            else{
+                great->next = head;
+                great = great->next;
+                head = head->next;
+                great -> next = NULL;
             }
+            
         }
         
-        return head;
+        less -> next  = great_head->next;
+        
+        return less_head->next;
+        
+        
+        // Approach 1 :  Brute O(n) .  Traverse and put elements in vector in order and then make list 
+//         ListNode* temp  = head;
+//         vector<int> krina ; 
+        
+        
+//         while(temp!= NULL){
+//             krina.push_back(temp->val);
+//             temp = temp ->next; 
+//         }
+        
+//         temp = head;
+//         for(int  i = 0 ; i < krina.size();i++){
+//             if(krina[i]<x){
+//                 temp->val = krina[i];
+//                 temp = temp->next;
+//             }
+//         }
+//         for(int  i = 0 ; i < krina.size();i++){
+//             if(krina[i]>=x){
+//                 temp->val = krina[i];
+//                 temp = temp->next;
+//             }
+//         }
+        
+//         return head;
         
     }
 };

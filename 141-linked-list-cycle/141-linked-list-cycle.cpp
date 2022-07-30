620ae5e0823183e0cf7303a025266253b5570fc6
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-30 18:06:01 +0530

    Time: 10 ms (89.85%), Space: 8.1 MB (57.63%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,24 +9,21 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        
         if(head == NULL)
             return false;
-
+        
         ListNode *fast = head;
         ListNode *slow = head;
         
-          while(fast != NULL && fast ->next != NULL)
-          {
-              fast = fast->next->next;
-              slow = slow->next;
+        while(fast !=NULL && fast->next !=NULL){
+            fast = fast->next->next;
+            slow= slow->next;
             
-			
-                // At the point if fast and slow are at same address
-                // this means linked list has a cycle in it.
-               if(fast == slow)
-                   return true;
-          }
-        // if traversal reaches to NULL this means no cycle.
+            if(fast==slow)
+                return true;
+        }
         return false;
+        
     }
 };

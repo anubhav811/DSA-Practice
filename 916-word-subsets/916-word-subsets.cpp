e3682ed9dea72e17c1ac6530b870b8b08ac812ea
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-30 11:48:22 +0530

    Time: 346 ms (43.82%), Space: 102.4 MB (40.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -4,21 +4,30 @@ public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         
        vector<int>freq(26,0);           
-        for(auto x:words2){
+        for(auto word:words2){
             vector<int>temp(26,0);
-            for(auto y:x){
-                temp[y-'a']++;
-                freq[y-'a'] = max(freq[y-'a'],temp[y-'a']); 
+            for(auto letter:word){
+                temp[letter-'a']++;
+                freq[letter-'a'] = max(freq[letter-'a'],temp[letter-'a']); 
             }
         }
         vector<string>res;
         for(auto x:words1){
             vector<int>temp(26,0);
-            for(auto y:x) temp[y-'a']++;    
+            for(auto y:x) 
+            {   
+                temp[y-'a']++;
+            }
             bool flag=true;
-            for(int i=0 ; i<26 ; i++)
-                if(freq[i]>temp[i]) {flag=false;break;}         
-            if(flag) res.push_back(x);
+            for(int i=0 ; i<26 ; i++){
+                if(freq[i]>temp[i]) {
+                    flag=false;
+                    break;
+                }         
+            }
+            if(flag) {
+                res.push_back(x);
+            }
         }
         return res;
     

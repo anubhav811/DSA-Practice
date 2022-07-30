eb4b9d80709db3ea95778c891280b62ae479345f
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-30 17:53:44 +0530

    Time: 92 ms (25.80%), Space: 14.5 MB (88.46%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -14,87 +14,96 @@ public:
         
         
         // Naive Approach 1:  Brute Force TC: O(m*n)  SC : O(1)
-//         while(head2 != NULL) {
-//         ListNode* temp = head1;
-//         while(temp != NULL) {
-//             //if both nodes are same
-//             if(temp == head2) return head2;
-//             temp = temp->next;
-//         }
-//         head2 = head2->next;
-//         }
-//         //intersection is not present between the lists return null
-//         return NULL;
-    
-        // Naive Approach 2: Hashing 
-        unordered_set<ListNode *> st;
-        while(head1!=NULL)
-        {
-            st.insert(head1);
-            head1=head1->next;
-        }
-        while(head2!=NULL){
-            if(st.find(head2) != st.end()){
-                return head2;
-            }
-            head2 = head2->next;
-        }
-        return NULL;
-        
-        /// OPTIMAL APPROACH 2  : Shift to other's head
- 
-//     	ListNode *ptr1 = headA;
-// 		ListNode *ptr2 = headB;
-// 		while(ptr1 != ptr2){
-// 			if(ptr1 == NULL){
-// 				ptr1 = headB;
-// 			}
-// 			else{
-// 				ptr1 = ptr1 -> next;
-// 			}
-// 			if(ptr2 == NULL){
-// 				ptr2 = headA;
-// 			}
-// 			else{
-// 				ptr2 = ptr2 -> next;
-// 			}
-// 		}
-// 		return ptr1;
+
+        //         while(head2 != NULL) {
+        //         ListNode* temp = head1;
+        //         while(temp != NULL) {
+        //             //if both nodes are same
+        //             if(temp == head2) return head2;
+        //             temp = temp->next;
+        //         }
+        //         head2 = head2->next;
+        //         }
+        //         //intersection is not present between the lists return null
+        //         return NULL;
 
         
-        /// OPTIMAL APPROACH 1  : Difference of len 
-		// int n = 0;
-		// int m = 0;
-		// ListNode* ptr1 = headA;
-		// ListNode* ptr2 = headB;
-		// while(ptr1 != NULL){
-		// 	n++;
-		// 	ptr1 = ptr1 -> next;
-		// }
-		// while(ptr2 != NULL){
-		// 	m++;
-		// 	ptr2 = ptr2 -> next;
-		// }
-		// int t = abs(n - m);
-		// if(n > m){
-		// 	while(t){
-		// 		headA = headA -> next;
-		// 		t--;
-		// 	}
-		// }
-		// else{
-		// 	while(t){
-		// 		headB = headB -> next;
-		// 		t--;
-		// 	}
-		// }
-		// while(headA != NULL and headB != NULL){
-		// 	if(headA == headB){
-		// 		return headA;
-		// 	}
-		// 	headA = headA -> next;
-		// 	headB = headB -> next;
-		// }
-		// return NULL;
+        
+        // Naive Approach 2: Hashing  TC : O(n + logn) SC : O(n)
+
+        //          unordered_set<ListNode *> st;
+        //          while(head1!=NULL)
+        //          {        
+        //              st.insert(head1);
+        //              head1=head1->next;
+        //          }
+        //          while(head2!=NULL){
+        //              if(st.find(head2) != st.end()){
+        //                  return head2;
+        //              }
+        //              head2 = head2->next;
+        //          }
+        //          return NULL;
+        
+        
+        // Optimal Approach 1  : Difference of len TC: O(n) SC: O(1)
+
+		         int n = 0;
+		         int m = 0;
+		         ListNode* ptr1 = head1;
+		         ListNode* ptr2 = head2;
+		         while(ptr1 != NULL){
+			            n++;
+			            ptr1 = ptr1 -> next;
+		         }
+		         while(ptr2 != NULL){
+        	            m++;
+        	            ptr2 = ptr2 -> next;
+		         }
+		         int t = abs(n - m);
+		         if(n > m){
+			            while(t){
+				            head1 = head1 -> next;
+				            t--;
+			             }
+		         }
+		         else{
+		 	        while(t){
+				            head2 = head2 -> next;
+				            t--;
+			              }
+		          }
+		         while(head1 != NULL and head2 != NULL){
+			            if(head1 == head2){
+				            return head1;
+			             }
+        	            head1 = head1 -> next;
+        	            head2 = head2 -> next;
+		         }
+		          
+                 return NULL;
+        
+        
+        // Optimal Approach 2  : Shift to other's head
+ 
+        //     	    ListNode *ptr1 = headA;
+        // 		    ListNode *ptr2 = headB;
+        // 		    while(ptr1 != ptr2){
+        // 			    if(ptr1 == NULL){
+        // 				    ptr1 = headB;
+        // 			    }   
+        // 			    else{
+        // 				    ptr1 = ptr1 -> next;
+        // 			    }
+        // 			    if(ptr2 == NULL){
+        // 				    ptr2 = headA;
+        // 			    }
+        // 			    else{
+        // 				    ptr2 = ptr2 -> next;
+        // 			    }
+        // 		    }
+        // 		    return ptr1;
+
+       
 	}
 };

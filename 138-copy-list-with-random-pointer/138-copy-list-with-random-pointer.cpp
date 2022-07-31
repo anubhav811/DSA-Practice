5eec429fba2818b39c385f42f0dd78aba772c2b2
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-07-31 16:32:27 +0530

    Time: 16 ms (41.06%), Space: 11.6 MB (7.22%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -18,19 +18,32 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
+        
+        
+        // Approach 1: HASMAP\
+        
         map<Node*, Node*> m;
         
-        Node* ptr = head;
-        while (ptr) {
-            m[ptr] =new Node(ptr->val);
-            ptr = ptr->next;
+        Node* itr = head;
+        
+        // in the map make key as the curr node and its copy as the value.
+        // ONLY COPY THE val not the two pointers
+        while (itr) {
+            
+             m[itr] =new Node(itr->val);  //key = itr and value = new Node(itr->val)
+           
+            itr = itr->next;
         }
-        ptr = head;
-        while (ptr) {
-            m[ptr]->next = m[ptr->next];
-            m[ptr]->random = m[ptr->random];
-            ptr = ptr->next;
-        }
+        // Keeping itr back to start(head)
+        itr = head;
+        
+        // iterate and then assign pointers to the copy by using map
+        while (itr!=NULL) {
+            
+            m[itr]->next = m[itr->next];
+            m[itr]->random = m[itr->random];
+            itr=itr->next;
+       }
         return m[head];
 
         

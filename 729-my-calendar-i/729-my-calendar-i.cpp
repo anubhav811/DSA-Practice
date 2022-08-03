e580a6ee4f9290a70bf9f530624cd056517dbc7e
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-08-03 19:52:24 +0530

    Time: 148 ms (63.09%), Space: 38.8 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,17 +1,33 @@
 class MyCalendar {
 public:
-    unordered_map<int, int>bookings;
+    // Approach 2 : Use ordered set to reduce the comparison.
+    
+    set<pair<int, int>>bookings;
     MyCalendar() {
     }
-    bool book(int start1, int end1) {
-            for(auto [start2, end2] : bookings) 
-                if(!(start1 >= end2 || start2 >= end1))   
-        // if neither of above condition is satisfied, there exists an intersection
-                    return false; 
-            bookings[start1] = end1;
-        // no intersection found, so insert the new slot and return true
-            return true;
-        }
+    bool book(int start, int end) {    
+       auto slot = bookings.lower_bound({start,end});
+        if( slot != bookings.end()   && !(slot -> first >= end) ||   
+            slot != bookings.begin() && !(prev(slot) -> second <= start)) 
+			    return false;         
+        bookings.insert({start, end});
+        return true;
+    }
+    
+    // APPROACH 1 :  Use map to store values then compare
+
+    // unordered_map<int, int>bookings;
+    // MyCalendar() {
+    // }
+    // bool book(int start1, int end1) {
+    //         for(auto [start2, end2] : bookings) 
+    //             if(!(start1 >= end2 || start2 >= end1))   
+    //     // if neither of above condition is satisfied, there exists an intersection
+    //                 return false; 
+    //         bookings[start1] = end1;
+    //     // no intersection found, so insert the new slot and return true
+    //         return true;
+    //     }
 };
 
 /**

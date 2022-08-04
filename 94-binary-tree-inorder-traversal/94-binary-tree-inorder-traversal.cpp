a1135bcc0dff4ec4c456591a1b14b3811de9ba04
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-08-04 23:17:08 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (74.76%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -31,22 +31,62 @@ public:
         // solveRecur(root,main);
         // return main;
         
-        // Iterative Solution
+        // Iterative Solution  O(n) O(n)
         
-        vector<int> inorder;
-        stack<TreeNode*> st;
-        while(root || !st.empty()){
-            if(root!=NULL){
-                st.push(root);
-                root=root->left;
+        // vector<int> inorder;
+        // stack<TreeNode*> st;
+        // while(root || !st.empty()){
+        //     if(root!=NULL){
+        //         st.push(root);
+        //         root=root->left;
+        //     }
+        //     else{
+        //         root = st.top();
+        //         st.pop();
+        //         inorder.push_back(root->val);
+        //         root=root->right;
+        //     }
+        // }
+        // return inorder;
+    
+//          Morris Traversal O(n) O(1)
+        vector<int> inorder; 
+        TreeNode *curr = root;
+        while(curr!=NULL){
+            if(curr->left == NULL){
+                // means that the their is no left and hence this curr is a root itself , thus add it
+                inorder.push_back(curr->val);
+                // now i have moved all the way left , printed root of the left subtree , now we go right
+                curr = curr->right;
             }
             else{
-                root = st.top();
-                st.pop();
-                inorder.push_back(root->val);
-                root=root->right;
+                // left subtree exists
+                // we will go to rightmost node of this subtree's left subtree (inorder ke hisaab se the last node to be visited of this subtree)
+                // we will have to save the current node and then traverse 
+                TreeNode *prev = curr->left;
+                
+                // the condition to be checked is that right exists and it doesnt point to curr (already)
+                while(prev->right != NULL && prev->right != curr){
+                    // keep going right
+                    prev=prev->right;
+                }
+                
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    // making the thread from righmost node to curr
+                    // next time when the curr reaches the same node this condition will fail go in else block
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = NULL;
+                    // now the node is itself the root , so it will be taken
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+                
             }
         }
+        
         return inorder;
         
     }

2012d971aadfb7f892e16462f1a984533c9760f6
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2022-08-05 15:54:15 +0530

    Time: 23 ms (44.21%), Space: 20.9 MB (81.30%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,7 +11,42 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root){
+   
+    // Approach 2 : Optimised maxDepth function 
+// TC : O(N)  . 
+    
+    int check(TreeNode* root){
+          if(root==NULL){
+            return 0;
+        }
+        
+// If either of left subtree or right subtree is unbalanced we wil return -1
+        
+        int lh = check(root->left);
+        if(lh == -1 )
+            return -1;
+        
+        int rh = check(root->right);
+        if(rh == -1 )
+            return -1;
+        
+        // checking unbalance for the current node
+        if(abs(lh-rh)>1)
+            return -1;
+        
+        // finally returning the height
+        return max(lh,rh) + 1 ;
+    }
+    
+    bool isBalanced(TreeNode* root){
+        return check(root) != -1;
+    }
+    
+    
+// Approach 1 : Naive . using maxDepth function to find lh and rh
+// TC : O(N^2)  . The maxDepth functions are increasing the complexity to N^2 . In better approach 
+    
+/*    int maxDepth(TreeNode* root){
         
         if(root==NULL){
             return  0 ;
@@ -23,22 +58,32 @@ public:
         return 1 + max(max_l,max_r);
 
     }
+    
     bool isBalanced(TreeNode* root) {
+        
         if(root==NULL){
             return true;
         }
        
+        // finding left and right height from the root node
         int lh = maxDepth(root->left);
         int rh = maxDepth(root->right);
         
+        // checking for un-balance condition
         if(abs(lh-rh)>1) return false;
+        
+        // above line did not execute means the tree is balanced from the root node perspspective , now we have to check for other nodes as well;
+        // so first we move left then right
         
         bool left = isBalanced(root->left);
         bool right = isBalanced(root->right);
+        
+        // if both the variables left and right are true it means that tree is balanced for every node 
         
         if(!left || !right){
             return false;
         }
         return true;
     }
+*/
 };

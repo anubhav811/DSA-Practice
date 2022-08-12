c4fabf78db3ced2e4e5f6b90ca447563f6ce7dbd
Junior IT Undergrad at VIT Vellore <71807967+anubhav811@users.noreply.github.com>
2022-08-12 20:36:01 +0530

    Time: 39 ms (56.64%), Space: 27.7 MB (68.42%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -13,21 +13,18 @@ class Solution {
 public:
     int maxi = INT_MIN;
     int solveRecur(TreeNode* root){
-        if(root==NULL){
-            return  0 ;
-        }
+        if(root==NULL) return  0 ;
+        
         int max_l = max(0,solveRecur(root->left));
         int max_r = max(0,solveRecur(root->right));
         // why taking max with return value and zero?
         // -> because the function might return a negative value and there is not point of adding it to the maxi variable because a negative value would never give a "max" sum
         
         maxi = max(maxi,root->val+max_l+max_r);
-        cout<<maxi<<endl;
         return max(max_l,max_r)+root->val;
     }
     int maxPathSum(TreeNode* root) {
         solveRecur(root);
         return maxi;
-
     }
 };

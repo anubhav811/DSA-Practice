547417aa61c6db8f9075cbea9b6da4455a369663
Junior IT Undergrad at VIT Vellore <71807967+anubhav811@users.noreply.github.com>
2022-09-20 00:29:43 +0530

    Time: 617 ms (5.72%), Space: 72.6 MB (5.34%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    void removeDupWord(string str,vector<string> &s)
+    {
+    istringstream ss(str);
+ 
+    string word; 
+    while (ss >> word)
+    {
+        s.push_back(word);
+    }
+}
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        
+        vector<vector<string>> res;
+        map<string,set<string>> mp;
+        
+        
+        for(auto it : paths){
+            vector<string> s ;
+            removeDupWord(it,s);
+            vector<string> files(s.size(),"");
+            copy(s.begin(),s.end(),files.begin());
+            
+            string mainRoot = files[0];
+            
+            for(int i=1 ;i<files.size();i++){
+                
+                string t = files[i];
+                
+                cout<<t<<endl;
+                
+                unsigned first = t.find("(");
+                unsigned last = t.find_last_of(")");
+                string content = t.substr (first+1,last-first-1);
+                
+                cout<<content<<endl;
+                
+                unsigned last2 = t.find("(");
+                string fileName = t.substr (0,last2);
+                
+                                cout<<fileName<<endl;
+
+                mp[content].insert(mainRoot+"/"+fileName);
+            }
+        }
+        
+        for(auto it: mp){
+            vector<string> paths(it.second.begin(),it.second.end());
+            if(paths.size()>1)
+                res.push_back(paths);
+        }
+        return res;
+    }
+};

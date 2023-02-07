a950ef48c76655049b6bdde45e96533cf318c560
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-02-07 13:36:18 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -7,41 +7,32 @@ using namespace std;
 
 class Solution {
   public:
-    /*  Function to implement Bellman Ford
-    *   edges: vector of vectors which represents the graph
-    *   S: source vertex to start traversing graph with
-    *   V: number of vertices
-    */
+  
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
-        // Code here
-        
+
         vector<int> dist(V,1e8);
-        dist[S]=0;
+        dist[S]= 0;
         
-        for(int i=0 ; i<V ;i++){
-            for(auto it: edges){
-                int u = it[0];
-                int v = it[1];
-                int w = it[2];
-                
-                if(dist[u]!=1e8 && dist[u]+w<dist[v]){
-                    dist[v] = dist[u]+w;
+        for(int i=0;i<V-1;i++){
+        for(auto it:edges){
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            
+            if(w+dist[u]<dist[v] && dist[u]!=1e8){
+                dist[v] = w+dist[u];
                 }
             }
         }
-        
-        // last relaxation to check negative cycles
-        for(auto it: edges){
-                int u = it[0];
-                int v = it[1];
-                int w = it[2];
-                
-                if(dist[u]!=1e8 && dist[u]+w<dist[v]){
-                    return {-1};
-                }
-        }
+        for(auto it:edges){
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
             
-        
+            if(w+dist[u]<dist[v] && dist[u]!=1e8){
+                return {-1};
+            }
+        }        
         return dist;
     }
 };

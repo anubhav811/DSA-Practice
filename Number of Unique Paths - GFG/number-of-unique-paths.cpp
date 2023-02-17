c6db910f048255c8d4765a77e74a4e67c3124895
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-02-17 22:02:57 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -41,11 +41,11 @@ class Solution
     
     int tab(int n , int m){
         vector<vector<int>> dp(n,vector<int>(m,0));
-        
+    
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0 && j==0){
-                    dp[0][0] = 1;
+                dp[0][0] = 1;
                     continue;
                     
                 }

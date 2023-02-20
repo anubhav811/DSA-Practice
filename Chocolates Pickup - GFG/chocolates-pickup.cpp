0724eb316fbc57f2c758529082e69ad0f5725de9
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-02-21 01:40:45 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -113,6 +113,60 @@ class Solution {
         
         }
 
+    int spaceOpt(int n, int m, vector<vector<int>>& grid) {
+        
+        vector<vector<int>> front(m,vector<int> (m, 0));
+        vector<vector<int>> curr(m,vector<int> (m, 0));
+        
+        vector < vector < vector < int >>> dp(n, vector < vector < int >> (m, vector < int > (m, 0)));
+        
+          for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+              if (j1 == j2)
+                front[j1][j2] = grid[n - 1][j1];
+              else
+                front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            }
+          }
+        
+          //Outer Nested Loops for travering DP Array
+          for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+              for (int j2 = 0; j2 < m; j2++) {
+        
+                int maxi = INT_MIN;
+        
+                //Inner nested loops to try out 9 options
+                for (int di = -1; di <= 1; di++) {
+                  for (int dj = -1; dj <= 1; dj++) {
+        
+                    int ans;
+        
+                    if (j1 == j2)
+                      ans = grid[i][j1];
+                    else
+                      ans = grid[i][j1] + grid[i][j2];
+        
+                    if ((j1 + di < 0 || j1 + di >= m) ||
+                      (j2 + dj < 0 || j2 + dj >= m))
+        
+                      ans += -1e9;
+                    else
+                      ans += front[j1 + di][j2 + dj];
+        
+                    maxi = max(ans, maxi);
+                  }
+                }
+                curr[j1][j2] = maxi;
+              }
+            }
+            front = curr;
+          }
+        
+          return front[0][m - 1];
+        
+        }
+
 
     int solve(int n, int m, vector<vector<int>>& grid) {
         
@@ -127,8 +181,13 @@ class Solution {
         // return memo(0, 0, m - 1, n, m, grid, dp);
         
         
-        // Tabulation   
-        return tab(n,m,grid);
+        // Tabulation             TC : O(N*M*M)*9
+        //                        SC : O(N*M*M)
+        // return tab(n,m,grid);
+        
+        // Space Optimized
+        
+        return spaceOpt(n,m,grid);
     }
 };
 

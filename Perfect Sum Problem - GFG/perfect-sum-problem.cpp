2c228c364b27425cfe72e26021fa433e5cf54477
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-02-24 21:25:48 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    
+    int mod = 1e9+7;
+    int recur(int i , int sum , int arr[]){
+        
+        if(i==0){
+            if(sum==0 && arr[0]==0) return 2;
+            if(sum==0) return 1;
+            return 0;
+        }
+
+        int pick = (arr[i]<=sum) ? recur(i-1,sum-arr[i],arr) : 0;
+        int notPick = recur(i-1,sum,arr);
+        
+        return (pick+notPick);
+        
+    }
+    int memo(int i , int sum ,vector<int> &arr,vector<vector<int>> &dp){
+        
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
+        }
+       
+        if(i==0){
+            if(sum==0 && arr[0]==0) return 2;
+            if(arr[0]==sum || sum==0) return 1;
+            return 0;
+        }
+
+        int pick = (arr[i]<=sum) ? memo(i-1,sum-arr[i],arr,dp)%mod : 0;
+        int notPick = memo(i-1,sum,arr,dp)%mod;
+        
+        return dp[i][sum] = (pick+notPick)%mod;
+        
+    }
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+	   // Recursion                      TC : O(2^N), SC : O(N)
+	   // Doesnt give correct answer because this approach is tailored for positive integer, not 0 and negative
+	   // return recur(n-1,sum,arr)%mod;
+	   
+	   vector<int> a ;
+	   for(int i=0;i<n;i++){
+	       a.push_back(arr[i]);
+	   }
+	   // Memoization
+	   sort(a.begin(),a.end());
+	   reverse(a.begin(),a.end());
+	   
+	   vector<vector<int>> dp (n,vector<int>(sum+1,-1));
+	   return memo(n-1,sum,a,dp);
+    }
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends

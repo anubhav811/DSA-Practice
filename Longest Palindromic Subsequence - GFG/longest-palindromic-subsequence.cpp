5db3edb8a1ec13e428c0a4876511d1d45668c726
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-03-26 21:54:19 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -8,7 +8,11 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution {
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
     int spaceOpt(int x, int y, string s1, string s2)
     {
@@ -34,9 +38,9 @@ public:
 
         return prev[y];
     }
+
     int tab(int x, int y, string s1, string s2)
     {
-
         // Adding one extra to shift indices to one right
         vector<vector<int>> dp(x + 1, vector<int>(y + 1, 0));
 
@@ -46,6 +50,7 @@ public:
             {
                 // as we have shifted right once , we will have to do 1 minus index to fetch the char from string
                 // because string ke indexes thori shift kare the , kar bhi nahi skte lmao
+
                 if (s1[i - 1] == s2[j - 1])
                 {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
@@ -56,7 +61,6 @@ public:
                 }
             }
         }
-
         return dp[x][y];
     }
     int memo(int i1, int i2, string s1, string s2, vector<vector<int>> &dp)
@@ -79,7 +83,8 @@ public:
             return max(memo(i1 - 1, i2, s1, s2, dp), memo(i1, i2 - 1, s1, s2, dp));
         }
     }
-    int recur(int i1, int i2, string s1, string s2){
+    int recur(int i1, int i2, string s1, string s2)
+    {
         if (i1 < 0 || i2 < 0)
         {
             return 0;
@@ -92,20 +97,21 @@ public:
             return max(recur(i1 - 1, i2, s1, s2), recur(i1, i2 - 1, s1, s2));
         }
     }
-    int longestPalinSubseq(string s) {
+    int longestPalinSubseq(string s)
+    {
 
         string t = s;
-        reverse(t.begin(),t.end());
+        reverse(t.begin(), t.end());
 
         int x = s.size();
         int y = t.size();
         // Recursion
         // return recur(x-1,y-1,s,t);                       // TC : O(2^x *  2^y)
-                                                         // SC : O(X+Y)
+        // SC : O(X+Y)
 
         // Memoization
         // vector<vector<int>> dp(x,vector<int>(y,-1));         // TC : O(X *  Y)
-                                                            // SC : O(X*Y) + O(X+Y)
+        // SC : O(X*Y) + O(X+Y)
         // return memo(x-1,y-1,s,t,dp);
 
         // Tabulation                                       TC : O(X*Y)
@@ -117,6 +123,8 @@ public:
         return spaceOpt(x, y, s, t);
     }
 };
+
+
 
 //{ Driver Code Starts.
 

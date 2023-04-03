94e030980315ec37dcde46f7e6af3b6b30682a9d
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-04-03 13:32:46 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -59,6 +59,43 @@ class Solution{
         
         return dp[i][j]=0;
     }
+    
+    bool tab(int n , int m , string pat , string str){
+        
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        
+        dp[0][0] = true;
+        for(int j=1;j<m+1;j++) dp[0][j] = false;
+        for(int i=1;i<n+1;i++){
+            bool flag = true;
+            for(int x=1;x<=i;x++){
+                if(pat[x-1]!='*'){
+                    flag = false;
+                    break;
+                }
+            }
+            
+            dp[i][0] = flag;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                
+                if(pat[i-1] == str[j-1] || pat[i-1]=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(pat[i-1]=='*'){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+                
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
     int wildCard(string pat,string str)
     {
         int n = pat.size();
@@ -67,10 +104,14 @@ class Solution{
         
         // return recur(n-1,m-1,pat,str);
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
         
-        return (memo(n-1,m-1,pat,str,dp)) ? 1 : 0;
+        // return (memo(n-1,m-1,pat,str,dp)) ? 1 : 0;
+        
+        return tab(n,m,pat,str);
+        
     }
+    
 };
 
 //{ Driver Code Starts.

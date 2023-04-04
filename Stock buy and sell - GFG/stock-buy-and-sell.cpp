88a195fb81771c2eb76abd9f1d1d89e000fa221f
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-04-04 12:27:26 +0530

    Added solution - LeetHub

--- Stock buy and sell - GFG/stock-buy-and-sell.cpp
+++ Stock buy and sell - GFG/stock-buy-and-sell.cpp
@@ -8,16 +8,32 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
+/*
+In this GFG version we have to find all the segments where we can make a local maximal profit , multiple buy-sell pair are allowed 
+Check LC version for different variant.
+
+Approach :
+1) We put buy and sell pointer at 0.
+2)We check if next element is bigger than the current i , if it is then we can move sell ahead by 1 (implying that we can sell at a greater amount)
+3) If its not bigger , that means the current position of sell shows the max profit we can make in the particular segment , so now we reset buy and sell to i+1
+4) Before reseting just add the pair of buy,sell to the result array
+
+As the loop works till n-1
+we have to check step 3 and 4 for one last time after the loop ends.
+*/ 
+
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution{
 public:
-    //Function to find the days of buying and selling stock for max profit.
     vector<vector<int>> stockBuySell(vector<int> A, int n){
-        // code here
         
         vector<vector<int>> res;
         
         int buy = 0;
         int sell = 0;
+        
         for(int i=0;i<n-1;i++){
            if(A[i+1]>A[i]){
                sell++;
@@ -35,11 +51,11 @@ public:
             res.push_back({buy,sell});
         }
         
-      
         return res;
         
     }
 };
+
 
 //{ Driver Code Starts.
 

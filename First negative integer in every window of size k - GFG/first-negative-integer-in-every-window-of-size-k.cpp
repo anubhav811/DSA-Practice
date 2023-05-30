5d62127e9e2b10121a4e3a74a20a3b5929ad7668
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-05-30 09:55:11 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -32,27 +32,54 @@ int main() {
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
                                                  
-            queue<long long> q;
-            vector<long long> ans;
-            long long i = 0,j = 0;
+        
+        // Sliding Window
+        
+        int i=0;
+        int j=0;
+        queue<long long> q;
+        vector<long long> res;
+        
+        while(j<N){
+            if(A[j]<0)                    // Do the operation
+                q.push(A[j]);
             
-            while(j<N){
-                if(A[j]<0)
-                    q.push(A[j]);
-                if(j-i+1 < K){
-                    j++;
-                }
-                else if(j-i+1 == K){
-                    if(q.empty()) 
-                        ans.push_back(0);
-                    else{
-                        ans.push_back(q.front());
-                        if(q.front() == A[i])
-                            q.pop();
-                    }
-                    i++;
-                    j++;
-                }
+            if(j-i+1<K){                  // Window smaller
+                j++;
             }
-            return ans;
+            else if(j-i+1==K){            //  Window size achieved
+                if(q.empty())             // q empty means no negative in this window
+                    res.push_back(0);
+                    
+                else{
+                    res.push_back(q.front());         // not empty hence the first guy in q is the first neg
+                                                      // FIFO
+                    if(q.front()==A[i]) q.pop();      // if the first guy is also the leftmost (index i) guy
+                                                      // he needs to be taken out as he wont be needed in
+                                                      // next window
+                }
+                i++;              // Move the window
+                j++;              // Move the window
+            }
+        }
+        
+        return res;
+
+
+        // Brute force
+        // vector<long long> res;
+        // for(int i=0;i<=N-K;i++){
+        //     bool flag = false;
+        //     for(int j=i;j<i+K;j++){
+        //         if(A[j]<0){
+        //             flag = true;
+        //             res.push_back(A[j]);
+        //             break;
+        //         }
+        //     }
+        //     if(!flag) res.push_back(0);
+        // }
+        
+        // return res;
+        
  }

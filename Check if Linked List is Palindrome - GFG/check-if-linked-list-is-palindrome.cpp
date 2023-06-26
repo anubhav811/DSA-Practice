a35d98f92430da904b0864938786ff4d2df2aa84
Anubhav Chachra <71807967+anubhav811@users.noreply.github.com>
2023-06-26 19:24:22 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    Node* reverseLL(Node* head){
+        
+        if(!head or !head->next) return head;
+        
+        Node* prev = NULL;
+        Node* curr = head;
+        while(curr){
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+    bool isPalindrome(Node *head)
+    {
+        Node* itr = head;
+        int len = 0;
+        while(itr){
+            itr=itr->next;
+            len++;
+        }
+        // finding middle of LL;
+        Node* slow = head;
+        Node* fast = head;
+        
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        
+        Node* node = (len%2==0) ? reverseLL(slow) : reverseLL(slow->next) ;
+        
+        while(node and head){
+            if(head->data!=node->data){
+                return false;
+            }
+            head = head->next;
+            node = node->next;
+        }
+        
+        return true;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
